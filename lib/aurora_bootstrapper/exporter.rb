572e8fe068b4150af0e5d44d719f315d8c07108b
YinfangZ <yinfang.zhuang@outreach.io>
2021-06-21 12:03:04 -0700

    Pass in s3path to Notifier; add tests

--- lib/aurora_bootstrapper/exporter.rb
+++ lib/aurora_bootstrapper/exporter.rb
@@ -5,13 +5,18 @@ module AuroraBootstrapper
     attr_reader :client
 
     def initialize( client:, prefix: "", export_bucket:, blacklisted_tables: "", whitelisted_tables: "", blacklisted_fields: "" )
-      @match              = "#{prefix}.*"
-      @export_bucket      = export_bucket
-      @blacklisted_tables = blacklisted_tables.split(",")
-      @whitelisted_tables = whitelisted_tables.split(",")
-      @blacklisted_fields = blacklisted_fields.split(",")
-      @client             = client
-      @notifier           = Notifier.new
+      begin
+        @match              = "#{prefix}.*"
+        @export_bucket      = export_bucket
+        @blacklisted_tables = blacklisted_tables.split(",")
+        @whitelisted_tables = whitelisted_tables.split(",")
+        @blacklisted_fields = blacklisted_fields.split(",")
+        @client             = client
+        @notifier           = Notifier.new( s3_path: export_bucket )
+      rescue => e
+        AuroraBootstrapper.logger.error message: "Error in initializing exporter.", error: e
+        raise
+      end
     end
 
     def export!
